f736bfe9b06471644733739738253a61a183ebb7
danielvornicu <vdany2003@yahoo.com>
2021-03-11 15:26:29 +0100

    first commit

--- src/server/server.js
+++ src/server/server.js
@@ -17,6 +17,16 @@ const posts = require('./routes/post.routes');
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 
+
+//APP settings
+// Serve only the static files form the build directory
+app.use(express.static('build'));
+
+//Wait for a request to any path and redirect all of the requests to index.html
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
